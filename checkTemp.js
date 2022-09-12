a0b6d8314741c8079329ad53e73f8dea7454e736
Mqxsyy <113341234+Mqxsyy@users.noreply.github.com>
2022-09-12 12:00:09 +0300

    Update checkTemp.js

--- checkTemp.js
+++ checkTemp.js
@@ -1,1 +1,13 @@
-
+console.log(checkTemp(10))
+   
+function checkTemp(Temp)
+{
+  if (Temp < 20)
+    return -1
+  
+  else if (Temp < 40)
+     return 0
+  
+  else if (Temp < 60)
+     return 1
+}
